dba31445caf3cdd310ab43842fbfdc3c8d39111b
Jayce <luangerleon@gmail.com>
2020-10-21 16:15:50 -0400

    Update Help.js

--- commands/Others/Help.js
+++ commands/Others/Help.js
@@ -16,7 +16,7 @@ module.exports = new (class cmd {
         .addField(":fox: Animals", client.commands.filter(a => a.category == "animals").map(a => "``"+a.name[a.name.length - 1]+"``").join(" | "))
         .addField("<:hentai:532962268028076054> Hentai", client.commands.filter(a => a.category == "hentai").map(a => "``"+a.name[a.name.length - 1]+"``").join(" | "))
         .addField("🔞 Porn", client.commands.filter(a => a.category == "porn").map(a => "``"+a.name[a.name.length - 1]+"``").join(" | "))
-        .setFooter("Prefix "+client.prefix+" - Created By Jayce ψ#7279","https://cdn.discordapp.com/emojis/532964141225476106.gif?v=1?size=2048")
+        .setFooter("Prefix "+client.prefix+" - Made by 1816#1816 ","https://cdn.discordapp.com/emojis/532964141225476106.gif?v=1?size=2048")
         .setColor("602bff");
         //buildMessage( embed ).send()
         message.channel.send(embed)
